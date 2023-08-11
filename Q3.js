ee4eb3d4313afbb6c1335421b7853437d4aff279
RutujaWarungase <136895155+RutujaWarungase@users.noreply.github.com>
2023-08-11 23:03:39 +0530

    Add files via upload

---
+++ Q3.js
@@ -0,0 +1,7 @@
+var h=require('http');
+var server=h.createServer(function (req,res){
+res.writeHead(200,{'content-type':'text/html'});
+res.write("<h1> Hello from Node Js</h1>");
+res.end();
+});
+server.listen(9000,function(){ console.log("server started on 9000");});
